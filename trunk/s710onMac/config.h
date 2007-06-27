e799a12e1dc0f01d4161cac7634757a68e88d6aa
philippe.barthelemy <philippe.barthelemy@c8d5a49e-0a30-0410-a901-09a4849b322f>
2007-06-27 21:21:47 +0000

    xcode project for the Apple Mac
    git-svn-id: http://s710.googlecode.com/svn@10 c8d5a49e-0a30-0410-a901-09a4849b322f

---
+++ trunk/s710onMac/config.h
@@ -0,0 +1,127 @@
+/* include/config.h.  Generated from config.h.in by configure.  */
+/* include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the <gd.h> header file. */
+#define HAVE_GD_H 1
+
+/* Define if libgd supports png. */
+#define HAVE_GD_PNG 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if you have gd library. */
+#define HAVE_LIBGD 1
+
+/* Define if you have the png library. */
+#define HAVE_LIBPNG 1
+
+/* Define if USB support is enabled via libusb */
+/* #undef HAVE_LIBUSB */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the `strftime' function. */
+#define HAVE_STRFTIME 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <termios.h> header file. */
+#define HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Name of package */
+#define PACKAGE "s710"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "dave@daveb.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "s710"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "s710 0.20"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "s710"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.20"
+
+/* Define to 1 if the C compiler supports function prototypes. */
+#define PROTOTYPES 1
+
+/* Define if the default value is inadequate */
+#define S710_FILEDIR "~/Documents/Polar/raw"
+
+/* Define if the default value is inadequate */
+#define S710_HRM_MAX_HR 200
+
+/* Define if the default value is inadequate */
+#define S710_HRM_REST_HR 60
+
+/* Define if the default value is inadequate */
+#define S710_MAX_VALID_CAD 170
+
+/* Define if the default value is inadequate */
+#define S710_MAX_VALID_HR 206
+
+/* Enable if your platform does not honor termios VTIME/VMIN settings */
+//#define  S710_SERIAL_ALT_INTER_CHAR_TIMER_IMP 
+
+/* Enable if you want libusb to do bulk reads (suits 2.4 kernels) */
+/* #undef S710_USB_BULK_READ */
+
+/* Define to 1 if the `setvbuf' function takes the buffering type as its
+   second argument and the buffer pointer as the third, as on System V before
+   release 3. */
+/* #undef SETVBUF_REVERSED */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if your <sys/time.h> declares `struct tm'. */
+/* #undef TM_IN_SYS_TIME */
+
+/* Version number of package */
+#define VERSION "0.20"
+
+/* Define like PROTOTYPES; this can be used by system headers. */
+#define __PROTOTYPES 1
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
